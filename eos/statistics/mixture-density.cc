d822c1461ec023c102945a4a05222fddd9ab54e9
Eduardo Romero <eduardo.romero@tum.de>
2018-09-12 09:21:10 +0200

    [statistics] First update to Mixture Density

--- eos/statistics/mixture-density.cc
+++ eos/statistics/mixture-density.cc
@@ -1,1 +1,3 @@
 #include <"mixture-density.hh">
+
+
--- eos/statistics/mixture-density.hh
+++ eos/statistics/mixture-density.hh
@@ -2,11 +2,19 @@
 #define EOS_STATISTICS_MIXTURE_DENSITY_HH
 
 #include <eos/utils/density.hh>
+#include <vector>
 
-class MixtureDensity : public Density
+namespace eos
 {
+  class MixtureDensity : public Density
+  {
+    using MixtureDensityPtr = std::shared_ptr<MixtureDensity>;
+    void evaluate(){ //Take a Component, evaluate it and add it to a sum of values. Use shared pointer to go through all components.
+      
 };
 class Component : public MixtureDensity
 {
-  virtual void fnct() = 0;
+  virtual void evaluate() = 0;
 };
+
+#endif 
